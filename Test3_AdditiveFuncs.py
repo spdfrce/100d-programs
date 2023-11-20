e5ee1a2506f092eb36c3eef27a84746019753ecf
Oteng Debrah <debrah459@gmail.com>
2023-11-20 21:47:23 +0000

    Add files via upload

---
+++ Test3_AdditiveFuncs.py
@@ -0,0 +1,12 @@
+#examinig the use of simple additive functions
+
+def add_Nums():
+     num1 = input("Enter a Number from 1 to 10: ")
+     num2 = input ("Enter a Number from 20 to 30: ")
+
+     add_Nums = num1  + num2
+     print(f"The result is {add_Nums}.")
+
+fname = input("Enter your first name: ")
+lname = input("Ënter your last name: ")
+add_Nums()
